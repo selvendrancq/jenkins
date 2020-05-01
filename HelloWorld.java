0ba5bb6a4a856627106745093ebfeeedf7d3a0d6
selrenga <selvendran.rengasamy@publicissapient.com>
2020-05-01 13:04:26 +0530

    adding java file

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+ public static void main(String[] args){
+  System.out.println("*************** HelloWorld - Welcome to Jenkins Training****************");
+  }
+}/* 9fbef606107a605d69c0edbcd8029e5d */
